894cd116aaabdbb90bee0ccd470d5e36e6fd64c4
WendiF <wendi.fan@softwire.com>
2021-02-22 15:32:02 +0000

    konami

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function plusSlides(n) {
 }
 
 function showSlides(n) {
+    console.log("hi")
     var i;
     var slides = document.getElementsByClassName("slide");
     if (n > slides.length) {slideIndex = 1}
@@ -14,4 +15,35 @@ function showSlides(n) {
         slides[i].style.display = "none";
     }
     slides[n % slides.length].style.display = "block";
+}
+
+var input = [];
+var konami = ["arrowup", "arrowup", "arrowdown", "arrowdown", "arrowleft", "arrowright", "arrowleft", "arrowright", "b", "a"];
+document.addEventListener('keydown', event => {
+    if (event.key.toLowerCase() === konami[input.length]) {
+        input.push(event.key.toLowerCase());
+    } else {
+        input = [];
+    }
+    if (JSON.stringify(input)===JSON.stringify(konami)) {
+        getGIF()
+    }
+
+})
+
+var xhr = new XMLHttpRequest();
+
+let animals = ["dragon", "shark", "hamster", "bat", "seal", "penguin"];
+function getGIF() {
+    xhr.addEventListener('load', function () {
+        let url = JSON.parse(xhr.responseText)["data"][Math.floor(Math.random() * 25)].url;
+
+        alert('Konami Code activated! Redirecting you to cuteness.')
+        window.location.href = url;
+    })
+
+    let animal = animals[Math.floor(Math.random() * animals.length)]
+
+    xhr.open('GET', `https://api.giphy.com/v1/gifs/search?api_key=QCaVYsJunUh3N1tUajZTN9KgiVtqCAp0&q=cute%20${animal}&limit=25&offset=0&rating=g&lang=en`);
+    xhr.send();
 }
